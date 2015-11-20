97217d3d649cfd5e8c5912f00db3473686491e02
littleRice <lihuixin2405@163.com>
2015-11-20 16:01:05 +0800

    20151120-effect
    图片有前后错落感

--- js/carouel.js
+++ js/carouel.js
@@ -15,19 +15,25 @@ $(function () {
     }
     $li.each(function (index, v) {
       if (index < position) {
-        $(v).find('img').width(250);
+        $(v).find('img').width(200);
         $(v).css('left', index * 100);
         $(v).css('z-index', length - position + index);
+        $(v).css('bottom', (position - index + 2) * 10);
       } else if (index == position) {
-        $(v).find('img').width('200');
+        $(v).find('img').width(300);
         $(v).css('left', index * 100);
+        $(v).css('bottom', '20px');
         $(v).css('z-index', length - position + index);
       } else {
         $(v).find('img').width(200);
         $(v).css('left', 100 + index * 100);
+        $(v).css('bottom', (index - position + 2) * 10);
         $(v).css('z-index', length - index - 1);
       }
     });
   });
   $li.eq(showPosition).trigger('click');
+  $li.on('mousemove', function () {
+    console.log(1)
+  });
 });
